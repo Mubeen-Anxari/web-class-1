b2936083740264e5f53a502eac6eac6c425b9b15
Mubeen-Anxari <mubeenansari10001@gmail.com>
2023-08-04 17:09:44 +0500

    web-class-1

---
+++ index.js
@@ -0,0 +1,90 @@
+"use strict";
+// var num1: number = 89;
+// console.log(num1);
+// var num2: number = 39;
+// var num3: number = 59;
+// console.log(num3);
+// var num4: string = "Mubeen";
+// var userName: string = `Ammar ${num4}`
+// console.log(num4);
+// console.log(userName);
+// var userNum: number = 10;
+// console.log(typeof userNum);
+// var userName: string = "mubeen";
+// console.log(typeof userName);
+//                            Arithmatic Operator
+// var num1: number = 10;
+// var num2: number = 14;
+// var num3: number = 15;
+// console.log(num1 + num2 + num3);
+// console.log(num1 - num2 - num3);
+// console.log(num1 * num2 * num3);
+// console.log(num1 / num2 / num3);
+//                             Modulous OPertaor
+// var num1: number = 9;
+// var num2: number = 5;
+// var result: number = num1 % num2;
+// console.log(result);
+//                              Assignment Operator
+// var num2: number = 10;
+// num2 += 5;
+// console.log(num2);
+//                              Template string literal
+// var userName1: string = "Mubeen";
+// var userName2: string = " Ammar Bilal";
+// console.log("Hi MUBEEN ", `your friend is `, `${userName2}`);
+//                                Comparison OPerator
+// var num1: number = 2;
+// var num2: number = 5;
+// var num3: number = 8;
+// var result: number = num1 + num2 - num3 * num1 / num1;
+// console.log(result);
+// var num1: number = 2;
+// var num2: number = 5;
+// var output: boolean = num1 == num2;
+// console.log(output);
+// var num1: number = 2;
+// var num2: number = 5;
+// var output: boolean = num1 < num2;
+// console.log(output);
+// var num1: number = 2;
+// var num2: number = 5;
+// var output: boolean = num1 > num2;
+// console.log(output);
+// var num1: number = 2;
+// var num2: number = 5;
+// var output: boolean = num1 != num2;
+// console.log(output);
+//                            And OPerator
+// T && T => T
+// T && F => F
+// F && T => F
+// F && F => F
+//                            Or Operator
+// T || T => T
+// T || F => T
+// F || T => T
+// F || F => T
+//                             And OPerator
+// var output : boolean= 5==5 && 2==2
+// console.log(output);
+// var output : boolean= 5==5 && 3==2
+// console.log(output);
+// var num1: number = 2;
+// var num2: number = 3;
+// var result: number = num1 ** num2;
+// console.log(result);
+if (7 < 5) {
+    console.log("line1");
+    console.log("line2");
+    console.log("line3");
+    console.log("line4");
+    console.log("line5");
+}
+{
+    console.log("line6");
+    console.log("line7");
+    console.log("line8");
+    console.log("line9");
+    console.log("line10");
+}
